08a14169171816e96f53c34d8d3d683ac0c80b24
MikhailShapshay <99117547+MikhailShapshay@users.noreply.github.com>
2022-11-27 17:05:04 +0600

    Авторизация и регистрация Sanctum

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * @param LoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        if(!Auth::attempt($data)) {
+            return response()->json('Login error!', 401);
+        }
+
+        /** @var User $user */
+        $user = $request->user();
+
+        $token = $user->createToken('api')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function user(Request $request){
+        return $request->user()->only(['name', 'email']);
+    }
+
+    /**
+     * @param RegisterRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterRequest $request)
+    {
+
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+
+        /** @var User $user */
+        $user = User::create($data);
+
+        $token = $user->createToken('api')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+
+Route::prefix('v1')->group(function (){
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::group(['middleware' => 'auth:sanctum'], function (){
+        Route::get('/user', [AuthController::class, 'user']);
+    });
 });
+
+
